109a51f334bfafa59255c7ccfaccb896d3177ff1
luoxiaohua <luoxh@cloudna.cn>
2024-02-01 09:10:54 +0800

    颜色数据文件

--- mock/chartcolor.js
+++ mock/chartcolor.js
@@ -1,21 +1,4 @@
 module.exports = {
   chartColor: ["#FDDD8D", "#E3CDDA", "#B0CAC7", "#FEF4C9", "#9DBFA2", "#E16C3F", "#CDD796", "#C4AFD2", "#C2ABC0", "#98B994", "#CEAABD", "#9189A5", "#FCA95E", "#D68E8E", "#8EB152", "#47A49B", "#FCAE82", "#B08837", "#A6DDC2", "#E0B7A9", "#D966A5", "#2B6919", "#539725", "#ACD9E6", "#C8DCCC", "#D3D8D7", "#E3C264", "#F2B6B4", "#E3AFD5", "#EEEDBC", "#A5B8B3", "#AF7222", "#E6B9A2", "#B8CCE3", "#F9D8AA", "#74ADD1", "#99DD8B", "#D3D6BF", "#F7B5CE", "#AEC7E8", "#A6D398", "#0C7169", "#C79D98", "#9C2A76", "#62A3CB"],
-  celltyleColorData :{
-    "Exc_FEZF2": "#FDDD8D",
-    "Exc_LAMP5": "#E3CDDA",
-    "Exc_RORB": "#B0CAC7",
-    "Exc_THEMIS": "#FEF4C9",
-    "Inh_LAMP5": "#9DBFA2",
-    "Inh_PVALB": "#E16C3F",
-    "Inh_SST": "#CDD796",
-    "Inh_VIP": "#C4AFD2",
-    "Nnc_Astro": "#C2ABC0",
-    "Nnc_Micro": "#98B994",
-    "Nnc_OPC": "#CEAABD",
-    "Nnc_Oligo": "#9189A5",
-    "Exc_OPRK1": "#FCA95E",
-    "Inh_SNCG": "#D68E8E",
-    "Exc_PCP4": "#8EB152",
-    "Exc_SLC17A7": "#47A49B",
-  }
+  celltyleColorData :["#FDDD8D","#E3CDDA","#B0CAC7","#FEF4C9","#9DBFA2","#E16C3F","#CDD796","#C4AFD2","#C2ABC0","#98B994","#CEAABD","#9189A5","#FCA95E","#D68E8E","#8EB152","#47A49B",]
 }
